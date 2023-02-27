6c3dbe361d46ea5b0b9f838044de33e9e53856f0
sab148 <s.benassou@fz-juelich.de>
2023-02-27 22:25:18 +0100

    changes in sh file

--- parallel/srun_loader_timer.sh
+++ parallel/srun_loader_timer.sh
@@ -3,13 +3,20 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=96
-#SBATCH --output=/p/home/jusers/benassou1/juwels/benassou1/2023-getting-started-with-ai-on-supercomputers/parallel/output/%j.out
-#SBATCH --error=/p/home/jusers/benassou1/juwels/benassou1/2023-getting-started-with-ai-on-supercomputers/parallel/output/%j.err
+#SBATCH --output=serial/output/%j.out
+#SBATCH --error=serial/output/%j.err
 #SBATCH --time=02:00:00
-#SBATCH --partition=develbooster
+#SBATCH --partition=booster
 #SBATCH --gres=gpu:1
-#SBATCH --account=atmlaml
+#SBATCH --account=training2303
 
+# Use this on tuesday
+##SBATCH --reservation=training-20230229
+# And this one on wednesday
+##SBATCH --reservation=training-20230301
+
+mkdir -p serial/output
 source sc_venv_template/activate.sh
+
 
 srun python -u loader_timer.py
