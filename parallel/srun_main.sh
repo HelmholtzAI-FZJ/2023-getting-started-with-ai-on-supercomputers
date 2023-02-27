e1147324b5024361835de3e944ab80c0638255ff
Alexandre Strube <a.strube@fz-juelich.de>
2023-02-27 21:37:46 +0100

    Change account to training, remove sabrina's paths

--- parallel/srun_main.sh
+++ parallel/srun_main.sh
@@ -3,13 +3,14 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=96
-#SBATCH --output=/p/home/jusers/benassou1/juwels/benassou1/2023-getting-started-with-ai-on-supercomputers/parallel/output/%j.out
-#SBATCH --error=/p/home/jusers/benassou1/juwels/benassou1/2023-getting-started-with-ai-on-supercomputers/parallel/output/%j.err
+#SBATCH --output=serial/output/%j.out
+#SBATCH --error=serial/output/%j.err
 #SBATCH --time=02:00:00
-#SBATCH --partition=develbooster
+#SBATCH --partition=booster
 #SBATCH --gres=gpu:1
-#SBATCH --account=atmlaml
+#SBATCH --account=training2303
 
+mkdir -p serial/output
 source sc_venv_template/activate.sh
 
-srun python -u main.py --data_dir "tiny-imagenet-200/" --log "/p/home/jusers/benassou1/juwels/benassou1/2023-getting-started-with-ai-on-supercomputers/parallel/logs/" 
+srun python -u main.py --data_dir "tiny-imagenet-200/" --log "serial/logs/" 
--- parallel/srun_main_multigpu.sh
+++ parallel/srun_main_multigpu.sh
@@ -3,13 +3,15 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=4
 #SBATCH --cpus-per-task=24
-#SBATCH --output=/p/home/jusers/benassou1/juwels/benassou1/2023-getting-started-with-ai-on-supercomputers/parallel/output/%j.out
-#SBATCH --error=/p/home/jusers/benassou1/juwels/benassou1/2023-getting-started-with-ai-on-supercomputers/parallel/output/%j.err
+#SBATCH --output=parallel/output/%j.out
+#SBATCH --error=parallel/output/%j.err
 #SBATCH --time=02:00:00
 #SBATCH --partition=develbooster
 #SBATCH --gres=gpu:4
-#SBATCH --account=atmlaml
+#SBATCH --account=training2303
 
+
+mkdir -p parallel/output
 source sc_venv_template/activate.sh
 
-srun python -u main_multigpu.py --data_dir "tiny-imagenet-200/" --log "/p/home/jusers/benassou1/juwels/benassou1/2023-getting-started-with-ai-on-supercomputers/parallel/logs/" 
+srun python -u main_multigpu.py --data_dir "tiny-imagenet-200/" --log "parallel/logs/" 
