5d5efba87e595087385eb7018ecfebe1d4699944
Sabrina Narimene Benassou <benassou1@jsfl01.jusuf>
2023-02-28 16:31:41 +0100

    update sbatch file

--- parallel/srun_main.sh
+++ parallel/srun_main.sh
@@ -13,14 +13,12 @@ mkdir -p slurm_outputs/
 #SBATCH --error=slurm_outputs/err-%j.err
 
 
-
-
 # Use this on tuesday
 ##SBATCH --reservation=training-20230229
 # And this one on wednesday
 ##SBATCH --reservation=training-20230301
 
-mkdir -p serial/output
+
 source sc_venv_template/activate.sh
 
 srun python -u main.py --log "logs/" 
