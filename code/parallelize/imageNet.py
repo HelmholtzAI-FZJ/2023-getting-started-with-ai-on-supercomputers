77ebe95702a1eb2f90643fbd3d5d8c43278bb2a7
sab148 <s.benassou@fz-juelich.de>
2023-09-22 18:04:46 +0200

    imagenet class updates

--- code/parallelize/imageNet.py
+++ code/parallelize/imageNet.py
@@ -31,27 +31,3 @@ class ImageNet(Dataset):
 
         return x, self.targets[idx]
 
-
-
-@click.command()
-@click.option("--data_root", "-r")
-def main(data_root):
-
-    transform = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Resize((256, 256))
-        ])
-
-    image_datasets = ImageNet(data_root, transform) 
-    dataloaders = DataLoader(image_datasets, batch_size=128, num_workers=int(os.getenv('SLURM_CPUS_PER_TASK')), pin_memory=True)
-
-    start_time = time.time()
-    for _ in dataloaders:
-        pass
-    end_time = time.time()
-    
-    td = (end_time - start_time)
-    print(time.strftime("%H:%M:%S", time.gmtime(td)))
-
-if __name__ == "__main__":
-    main()
